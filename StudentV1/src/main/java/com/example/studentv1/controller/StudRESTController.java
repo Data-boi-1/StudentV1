cc8315f22381555c3752f46191696d0ab8ac7588
Data-boi-1 <99009108+Data-boi-1@users.noreply.github.com>
2023-02-15 10:04:22 +0100

    student

--- StudentV1/src/main/java/com/example/studentv1/controller/StudRESTController.java
+++ StudentV1/src/main/java/com/example/studentv1/controller/StudRESTController.java
@@ -5,9 +5,7 @@ import com.example.studentv1.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -52,5 +50,20 @@ public class StudRESTController {
      return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
    }
   }
+
+  @PostMapping("/student")
+  @ResponseStatus(HttpStatus.CREATED)
+  public Student postStudent(@RequestBody Student student){
+    System.out.println(student);
+    return studentrepository.save(student);
+  }
+
+  @DeleteMapping("/deletestudent/{id}")
+  public ResponseEntity<Student> deleteStudent(@PathVariable Integer id){
+    studentrepository.deleteById(id);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+
   }
 
